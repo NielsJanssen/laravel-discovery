80a0d9b1254c1da9b2064bc2ae104473974812cb
Niels Janssen <niels@bttr.nl>
2026-05-21 22:19:56 +0200

    refactor: replace CommandRegistry with CommandDecorator
    Rename CommandRegistry to CommandDecorator, narrowing its responsibility to wrapping command definitions rather than also owning the command list and registration logic. Move the commands collection into CommandDiscovery and inline the kernel registration loop in the afterResolving callback.

--- src/Feature/Command/CommandDiscovery.php
+++ src/Feature/Command/CommandDiscovery.php
@@ -14,61 +14,56 @@ use Tempest\Discovery\DiscoveryLocation;
 use Tempest\Discovery\IsDiscovery;
 use Tempest\Discovery\SkipDiscovery;
 use Tempest\Reflection\ClassReflector;
-use Tempest\Reflection\MethodReflector;
 
 #[SkipDiscovery]
 final class CommandDiscovery implements Discovery, Feature
 {
     use IsDiscovery;
 
-    public function __construct(
-        private readonly CommandRegistry $store,
-    ) {}
+    /** @var list<\NielsJanssen\Laravel\Discovery\Feature\Command\CommandDefinition> */
+    private(set) array $commands = [];
 
     public function discover(DiscoveryLocation $location, ClassReflector $class): void
     {
         if ($class->is(LaravelCommand::class)
-            || $class->hasAttribute(Command::class)) {
-            $this->discoveryItems->add($location, $class);
+            || ($definition = $class->getAttribute(Command::class))) {
+            $this->discoveryItems->add($location, [$class, $definition ?? null]);
 
             return;
         }
 
         foreach ($class->getPublicMethods() as $method) {
-            if ($method->hasAttribute(Command::class)) {
-                $this->discoveryItems->add($location, $method);
+            if ($definition = $method->getAttribute(Command::class)) {
+                $this->discoveryItems->add($location, [$method, $definition]);
             }
         }
     }
 
     public function apply(): void
     {
-        foreach ($this->discoveryItems as $reflector) {
-            if ($reflector instanceof ClassReflector) {
-                $this->store->commands[] = new CommandDefinition(
-                    reflector: $reflector,
-                    definition: $reflector->getAttribute(Command::class),
-                );
-            }
-
-            if ($reflector instanceof MethodReflector) {
-                foreach ($reflector->getAttributes(Command::class) as $definition) {
-                    $this->store->commands[] = new CommandDefinition(
-                        reflector: $reflector,
-                        definition: $definition,
-                    );
-                }
-            }
+        foreach ($this->discoveryItems as [$reflector, $definition]) {
+            $this->commands[] = new CommandDefinition(
+                reflector: $reflector,
+                definition: $definition,
+            );
         }
     }
 
     public static function register(Application $app, DiscoveryConfig $config): void
     {
         $app->singleton(__CLASS__);
-        $app->singleton(CommandRegistry::class);
+        $app->singleton(CommandDecorator::class);
 
-        $app->afterResolving(Kernel::class, function (Kernel $kernel) use ($app) {
-            $app->make(CommandRegistry::class)->register($kernel);
+        $app->afterResolving(Kernel::class, static function (Kernel $kernel, Application $app) {
+            $decorator = $app->make(CommandDecorator::class);
+
+            foreach ($app->make(__CLASS__)->commands as $command) {
+                $kernel->registerCommand(
+                    $command->definition
+                        ? $decorator->decorateCommand($command)
+                        : $app->make($command->reflector->getName()),
+                );
+            }
         });
     }
 }
