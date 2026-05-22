e7b3e90d78e71fdc56ac61560a64747d0292b48b
Niels Janssen <git@nielsjanssen.eu>
2026-05-22 12:02:42 +0200

    refactor: extract anonymous command class into DecoratedCommand
    Move the anonymous class from CommandDecorator into a dedicated DecoratedCommand class. Middleware is now resolved eagerly in the constructor, allowing middlewares that implement ProvidesInputOptions to register additional input options/arguments before execution. CommandMiddleware signature updated to accept a typed Command instead of object.

--- src/Feature/Command/CommandDecorator.php
+++ src/Feature/Command/CommandDecorator.php
@@ -7,8 +7,6 @@ namespace NielsJanssen\Laravel\Discovery\Feature\Command;
 use Illuminate\Console\Command as LaravelCommand;
 use Illuminate\Container\Attributes\Singleton;
 use Illuminate\Container\Container;
-use Tempest\Reflection\ClassReflector;
-use Tempest\Reflection\MethodReflector;
 
 #[Singleton]
 readonly class CommandDecorator
@@ -23,73 +21,6 @@ readonly class CommandDecorator
      */
     public function decorateCommand(CommandDefinition $command): LaravelCommand
     {
-        return new class ($this->container, $command) extends LaravelCommand {
-            private string $commandClass;
-
-            private CommandArgumentsDefinition $commandSignature;
-
-            private MethodReflector $commandMethodReflector;
-
-            public function __construct(
-                private readonly Container         $container,
-                private readonly CommandDefinition $definition,
-            ) {
-                $this->name = $definition->definition->name;
-
-                parent::__construct();
-
-                $this->setAliases($definition->definition->aliases);
-                $this->setDescription($definition->definition->description ?? '');
-
-                $reflector = $this->definition->reflector;
-
-                if ($reflector instanceof ClassReflector) {
-                    $this->commandClass           = $reflector->getName();
-                    $this->commandMethodReflector = $reflector->getMethod('__invoke');
-                } elseif ($reflector instanceof MethodReflector) {
-                    $this->commandClass           = $reflector->getDeclaringClass()->getName();
-                    $this->commandMethodReflector = $reflector;
-                } else {
-                    throw new \LogicException('Unsupported reflector type');
-                }
-
-                $this->commandSignature = CommandArgumentsDefinition::from($this->commandMethodReflector);
-                $this->commandSignature->define(
-                    $this->getDefinition(),
-                );
-            }
-
-            public function __invoke(): mixed
-            {
-                $instance = $this->container->make($this->commandClass, [
-                    'input'  => $this->input,
-                    'output' => $this->output,
-                ]);
-
-                $pipeline = function () use ($instance) {
-                    return $this->commandMethodReflector->getReflection()
-                        ->getClosure($instance)
-                        ->call($instance, ...$this->commandSignature->resolveInput($this->input));
-                };
-
-                foreach (array_reverse($this->definition->definition->middleware) as $middleware) {
-                    $next = $pipeline;
-
-                    if (is_string($middleware)) {
-                        $middleware = $this->container->make($middleware);
-                    }
-
-                    $pipeline = static function () use ($middleware, $instance, $next) {
-                        return $middleware($instance, $next);
-                    };
-                }
-
-                // TODO: #0 - Move to custom command class instead of an anonymous class
-                // TODO: #1 - Allow middleware to short-circuit the command execution by returning a non-null value.
-                // TODO: #2 - Add a way for middleware to add and consume input options/arguments, this may need a change in the CommandArgumentsDefinition run
-
-                return $pipeline();
-            }
-        };
+        return new DecoratedCommand($this->container, $command);
     }
 }
--- src/Feature/Command/Middleware/Transaction.php
+++ src/Feature/Command/Middleware/Transaction.php
@@ -4,6 +4,7 @@ declare(strict_types=1);
 
 namespace NielsJanssen\Laravel\Discovery\Feature\Command\Middleware;
 
+use Illuminate\Console\Command;
 use Illuminate\Support\Facades\DB;
 use NielsJanssen\Laravel\Discovery\Feature\Command\CommandMiddleware;
 
@@ -12,7 +13,7 @@ class Transaction implements CommandMiddleware
     /**
      * @throws \Throwable
      */
-    public function __invoke(object $command, callable $next): mixed
+    public function __invoke(Command $command, callable $next): mixed
     {
         return DB::transaction(static fn() => $next());
     }
